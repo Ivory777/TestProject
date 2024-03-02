95ea93a5d05331610ff5f79d6e0dc98a610ef287
able2008 <able2008@my.fit.edu>
2024-03-01 20:19:53 -0500

    Editing code for our example

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
-print("Hello world, from Python")
+print("Hello world")
 print("First change.")
 print("Second change.")
 for counter in range(1, 11):
     print(counter)
-
+print("This is another change")
